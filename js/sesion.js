a2cb45454f92fd9772b7030be4c71f21cebc1e50
Ulabarri-020 <119754546+Ulabarri-020@users.noreply.github.com>
2024-10-07 08:39:37 -0500

    actualizacion

--- js/sesion.js
+++ js/sesion.js
@@ -12,54 +12,7 @@ signInButton.addEventListener('click', () => {
 });
 
 /*validaciones js */
-function click() {
-    let nombre =document.getElementById('nombre').value;
-let correo =document.getElementById('correo').value;
-let telefono =document.getElementById('telefono').value;
-let contraseña =document.getElementById('contraseña').value;
-/*funcion para validar */
-function validar_parametros() {
-    if (nombre==null  || correo==null   || telefono== null    || contraseña==null  ) {
 
-        alert("debes ingresar todos los campos");
-
-    }
-    else if(contraseña<8    || contraseña>10){
-        alert("la cantidad de caracteres de estar entre 8 y 10 caracteres");
-        console.log(contraseña)
-        contraseña.value=contraseña;  
-        validarContraseña(contraseña);
-        limpiar();
-    }
- 
-   
-
-
-}
-
-}
-/*modal */
-
-function validarContraseña(contraseña) {
-    const tieneMayuscula = /[A-Z]/.test(contraseña);
-    const tieneMinuscula = /[a-z]/.test(contraseña);
-    const tieneNumero = /[0-9]/.test(contraseña);
-    const tieneCaracterEspecial = /[!@#$%^&*()_+\-=[\]{}|;:,.<>?/`~]/.test(contraseña);
-
-    if (tieneMayuscula && tieneMinuscula && tieneNumero && tieneCaracterEspecial) {
-        alert("Contraseña fuerte");
-    } else {
-        alert("Contraseña débil: Debe incluir al menos una mayúscula, una minúscula, un número y un carácter especial");
-    }
-}
-function limpiar(){
-
-    let nombre =document.getElementById('nombre').value=" ";
-    let correo =document.getElementById('correo').value=" ";
-    let telefono =document.getElementById('telefono').value=" ";
-  let contraseña =document.getElementById('contraseña').value=" ";
-
-}
     
 
 
