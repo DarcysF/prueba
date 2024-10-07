79bfa75ddb3adaf023a7e23f8852482324346a17
Alfredo Sandoval <sandovalalfredo376@gmail.com>
2024-10-07 09:16:21 -0500

    actualizacion

--- includes/User.php
+++ includes/User.php
@@ -19,7 +19,8 @@
         }
         // Registro de usuario
         public function register($name, $email, $telephone, $password, $rol = 2, $state = 2)
-        {
+        {     $num_rows=null;
+
             try {
                 // Validación básica
                 if (empty($name) || empty($email) || empty($telephone) || empty($password)) {
@@ -30,12 +31,12 @@
                 </div>
                 </div>
                     <script>
-        // Usando setTimeout para ejecutar una función después de 5 segundos.
-        setTimeout(function() {
-            // Redirigir con JavaScript
-            window.location.href = "/darcy/prueba/public/index.html";
-        }, 4000);
-    </script>
+                        // Usando setTimeout para ejecutar una función después de 5 segundos.
+                        setTimeout(function() {
+                            // Redirigir con JavaScript
+                            window.location.href = "/darcy/prueba/public/index.html";
+                        }, 4000);
+                    </script>
                 ');
                 } else {
                     // Insertar el usuario en la base de datos
@@ -52,21 +53,37 @@
                         ':id_state_user' => $state
                     ]);
                     echo (
-                        '<div id="responseMessage" class="respuesta" >
-                <p style="color: red;">Bienvenido a nuestra biblioteca virtual </p>
-                <div class="img">
-                 <img  src="/darcy/prueba/img/exito.gif" alt="icono de exito" srcset="">
-                </div>
-                </div>
-                <script>
-        // Usando setTimeout para ejecutar una función después de 5 segundos.
-        setTimeout(function() {
-            // Redirigir con JavaScript
-            window.location.href = "/darcy/prueba/public/home.html";
-        }, 5000);
-    </script>
+                                '<div id="responseMessage" class="respuesta" >
+                        <p style="color: red;">Bienvenido a nuestra biblioteca virtual </p>
+                        <div class="img">
+                        <img  src="/darcy/prueba/img/exito.gif" alt="icono de exito" srcset="">
+                        </div>
+                        </div>
+                        <script>
+                            // Usando setTimeout para ejecutar una función después de 5 segundos.
+                            setTimeout(function() {
+                                // Redirigir con JavaScript
+                                window.location.href = "/darcy/prueba/public/home.html";
+                            }, 5000);
+                        </script>
                 '
-                    );
+                           );
+                 /*funcion para validar datos  */
+                  $consulta_validar_datos="SELECT * FROM users WHERE name_user='$name' and mail_user='$email'
+                   and telephone_user = '$telephone'
+                   and password_user = '$password'
+                   and id_rol_user= '$rol'
+                   and id_state_user = '$state'";
+                  $validando= $this->db->getPDO()->prepare($consulta_validar_datos);
+                  if ($validando->$num_rows>0) {
+
+                    echo ("  <h1>el usuario ya existe </h1>");
+                  }
+                  else{
+                    echo ("  <h1>BIEN </h1>");
+
+                  }
+    
                 }
             } catch (PDOException $e) {
                 error_log('Error en el registro de usuario: ' . $e->getMessage());
@@ -125,6 +142,7 @@
         }
     }
     ?>
+  
 </body>
 
 </html>
